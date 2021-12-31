ac6100400d59d51ed6987fdd930a13760b6c2ed2
Jose Hernandez <jhernandez27603@gmail.com>
2021-12-31 18:30:42 -0500

    add posts Service to handle getting and adding posts

--- src/app/posts/post-list.component/post-list.component.ts
+++ src/app/posts/post-list.component/post-list.component.ts
@@ -1,17 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   { title: 'First Post', content: 'First post Content!' },
   //   { title: 'Second Post', content: 'Second post Content!' },
   //   { title: 'Third Post', content: 'Third post Content!' },
   // ];
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub!: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService
+      .getPostUpdateListener()
+      .subscribe((posts) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.postsSub) {
+      this.postsSub.unsubscribe();
+    }
+  }
 }
