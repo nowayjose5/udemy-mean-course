13865891120fe59a6690d79ec6deaf867e0c50cf
Jose Hernandez <jhernandez27603@gmail.com>
2022-01-01 15:21:46 -0500

    fetech posts from express backend

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
@@ -8,8 +9,17 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private httpClient: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.httpClient
+      .get<{ message: string; posts: Post[] }>(
+        'http://localhost:3000/api/posts'
+      )
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
